2cfddead6205be3009d7741100bb9305866d1b14
naturalbornlazy <natural_born_lazy@yahoo.com>
2017-01-07 00:14:10 +0200

    0.1
    base tile grid creation

---
+++ Hackers/Assets/Scripts/HexCell.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour {
+
+    public HexCoordinates coordinates;
+    public Color color;
+
+    //public Terrain terrain;
+    //public Unit unit;
+    //public Building building;
+    
+    // Use this for initialization
+    void Start () {
+        //Terrain = new Terrain();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Hackers/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+
+    public Text cellLabelPrefab;
+
+    public Vector3 menuOffset;
+
+    public Transform menuPos;
+
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+
+    HexCell[] cells;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+        
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        // generate cells matrix previously
+        // determine cells[i] type
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        
+        // switch (type)
+        // case terrain: cell.terrain = ...
+        // etc.
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+        
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+
+    public void ColorCell(Vector3 position, Color color)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        cell.color = color;
+        hexMesh.Triangulate(cells);
+
+        RectTransform rTrans = menuPos as RectTransform;
+        rTrans.anchoredPosition3D = new Vector3(cell.transform.position.x + menuOffset.x, cell.transform.position.z + menuOffset.y, menuOffset.z);
+        rTrans.SetAsLastSibling();
+    }
+
+}
